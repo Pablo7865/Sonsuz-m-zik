92495425533efd81cb093673e6d83aad582c14c4
Pablo7865 <brktyl58@gmail.com>
2026-08-11 08:26:18 +0300

    Update "index.js

--- "index.js
+++ "index.js
@@ -1,21 +1,14 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
-
 client.once("ready", () => {
   console.log("Bot aktif!");
 });
-
-client.on("messageCreate", message => {
+client.on("messageCreate", (message) => {
   if (message.content === "!ping") {
     message.reply("Pong!");
   }
 });
-
 client.login(process.env.TOKEN);
